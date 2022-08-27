52689810b8620eb71c89dfc622df5773cc24ee95
KingOnFire <67450938+KingOnFire@users.noreply.github.com>
2022-08-26 21:27:03 -0700

    Updated code + added other images and hw uploads

--- imagetobinary/main.py
+++ imagetobinary/main.py
@@ -1,27 +1,40 @@
-# Hello mr boneville i dont know if you are reading the code but yes i wrote this
-# I decided instead of making a mediocre image of a dime I would do this :)
+# Hello mr boneville i dont know if you are reading the code but yes i wrote this me kingston du
+# I decided instead of making a mediocre dime image Pixel Art I would do this :)
+
 
 print("hello world")
 
 from PIL import Image
 import pyperclip
+from sys import argv
 
-im = Image.open("image.jpg")
-pixels = im.load()
+imageName = ''
 
 def main():
+    im = Image.open(imageName)
+    pixels = im.load()
     binaryImage = ''
 
     for y in range(im.size[1]):
         for x in range(im.size[0]):
+
             #add to string the binary values
             binaryImage += toBinary(pixels[x,y][0])
             binaryImage += toBinary(pixels[x,y][1])
             binaryImage += toBinary(pixels[x,y][2])
 
+            if(argv[2]=='-bw' and not pixels[x,y][0]==pixels[x,y][1]==pixels[x,y][2]):
+                raise Exception("Not a black and white image")
+
+    #black and white option
+    if argv[2] == '-bw':
+        # image is all 00000000 or 111111111 turn it to just 1
+        binaryImage = binaryImage[0:len(binaryImage)-1:24]
+
     print(im.size)
     pyperclip.copy(binaryImage)
 
+#convert decimal to binary, and make it have 8 digits
 def toBinary(decimal):
     value = bin(decimal)[2:]
     if len(value) < 8:
@@ -32,5 +45,8 @@ def toBinary(decimal):
 
 
 if __name__ == '__main__':
+    imageName = argv[1]
+    if len(argv) < 3:
+        argv.append("-c")
     main()
 
